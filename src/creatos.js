023a931c5f5261b567d37a809e0ac90d46653ccb
Isabella Wang <35751984+isabellaWang0108@users.noreply.github.com>
2021-04-22 18:52:31 -0400

    init

---
+++ src/creatos.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const total = 100;
+var owner_count = 1;
+export default class Creator extends React.Component {
+
+    state = {
+        count: 0,
+        creators: [{ name: 'cryptogod', ownership: 100 }]
+    }
+
+
+    addCreator() {
+        owner_count++;
+        this.setState({
+            creators: [...this.state.creators, { name: 'cryptogod', ownership: ' ' }]
+        })
+    };
+
+    changeOwnership(event) {
+        //   change the current input
+        var newArr = this.state.creators;
+        newArr[event.target.id].ownership = event.target.value;
+        this.setState({ creators: newArr });
+
+        // change the calunter value
+        var newCount = 100;
+        for (var i = 0; i < this.state.creators.length; i++) {
+            newCount -= this.state.creators[i].ownership;
+        }
+        this.setState({ count: newCount });
+    }
+
+    changeName() {
+
+    }
+    render() {
+
+        return (
+            <div>
+                {this.state.creators.map((content, i) => {
+                    return <div className="form-row" key={i} style={{ marginBottom: 16 }}>
+                        <div className="col-md-4">
+                            {i === 0 ?
+                                <input type="name" className="form-control" value={content.name} readOnly />
+                                : <input type="name" className="form-control" placeholder="username" onChange={this.changeName.bind(this)} />
+                            }
+                        </div>
+                        <div className="col-md-3">
+                            {/* {i === 0 ?
+                                <input type="number" value={content.ownership} className="form-control" id="quantity" name="quantity" min="0" max="100" readOnly />
+                                : */}
+                            <input type="number" value={content.ownership} id={i} className="form-control" placeholder='00' onChange={this.changeOwnership.bind(this)} />
+                            {/* } */}
+                        </div>
+                        <div className="col-md-3"> % ownership </div>
+                    </div>
+                }
+                )}
+                {this.state.count === 0 ? <p></p> : <b style={this.state.count < 0 ? { color: 'red' } : { color: 'black' }}>You have {this.state.count} credits left. Total credies need to be 100.</b>}
+
+                <div style={{ cursor: 'pointer', color: 'blue' }} onClick={() => this.addCreator()} id="add_people">Add a collaborator +</div>
+                <br />
+                {this.state.count === 0 ? <button>submit</button> : <button disabled>submit</button>}
+            </div>
+        )
+
+    }
+}
